cf5604e1b6b25e3fe5207eb64e6d59b78ae0b1cc
KoTko (Konstantin Bagdasarov) <75670213+ZKoTko@users.noreply.github.com>
2024-08-26 21:00:31 +0400

    main

---
+++ js/accordion.js
@@ -0,0 +1,25 @@
+const accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
+
+accordionItemHeaders.forEach(accordionItemHeader => {
+accordionItemHeader.addEventListener("click", event => {
+    
+    // Only 1 showing (if you open the second one, first one would close)
+    
+    // const currentlyActiveAccordionItemHeader = document.querySelector(".accordion-item-header.active");
+    // if(currentlyActiveAccordionItemHeader && currentlyActiveAccordionItemHeader!==accordionItemHeader) {
+    //   currentlyActiveAccordionItemHeader.classList.toggle("active");
+    //   currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = 0;
+    // }
+
+    accordionItemHeader.classList.toggle("active");
+    const accordionItemBody = accordionItemHeader.nextElementSibling;
+
+    if(accordionItemHeader.classList.contains("active")) {
+        accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+    }
+    else {
+        accordionItemBody.style.maxHeight = 0;
+    }
+    
+    });
+});
